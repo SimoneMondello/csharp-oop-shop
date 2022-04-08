21987e1d72c320bc8c8bb8705aa55f8cd9dc0849
SimoneMondello <mondellosimo@gmail.com>
2022-04-08 16:43:59 +0200

    primo.commit

--- CShop/Class1.cs
+++ CShop/Class1.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace CShop
 {
-    internal class Class1
+    internal class prodotto
     {
+        private int Codice;
+        private string Nome;
+        private string Descrizione;
+        private int Prezzo;
+        private int Iva;
+
+        public prodotto(int codice, string nome, int prezzo, string descrizione, int iva)
+        {
+            this.Codice = codice;
+            this.Nome = nome;
+            this.Descrizione = descrizione;
+            this.Iva = iva;
+
+
+        }
+        public int NumeroCasuale()
+            {
+            Random r = new Random();
+            return r.Next(100000000);
+            }
+        public double CalcoloPrezzoIva()
+        {
+            double ivato = Prezzo * Iva;
+            double prezzoFinale = Prezzo + ivato / 100;
+            return prezzoFinale ;
+            Console.WriteLine("Prezzo finale " +prezzoFinale + ivato);
+        }
+        public void iva()
+        {
+            double Iva = 22 % Prezzo;
+        }
+        public void prezzo ()
+        {
+            Prezzo = 10;
+        }
+        public void StampaCodice()
+        {
+            Console.WriteLine ("Nome prodotto "+ NumeroCasuale() + Nome);
+        }
     }
+
+
+
+   
 }
--- CShop/Program.cs
+++ CShop/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+/* Nel progetto csharp-oop-shop, creare la classe Prodotto che gestisce i prodotti dello shop.
+Un prodotto è caratterizzato da:
+codice (numero intero)
+nome
+descrizione
+prezzo
+iva
+Usate opportunamente ai modificatori di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random.
+Il codice prodotto sia accessibile solo in lettura
+Gli altri attributi siano accessibili sia in lettura che in scrittura
+Il prodotto esponga sia un metodo per avere il prezzo base
+uno per avere il prezzo comprensivo di iva
+Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+Nel vostro programma principale, testate tutte 
+le funzionalità della classe Prodotto, creando vari prodotti e stampandone il loro contenuto.*/
